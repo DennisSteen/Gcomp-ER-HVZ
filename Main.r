4ee9a5fbbe666541be6a4a0014cd8d9c6702dba1
DennisSteen <103998133+DennisSteen@users.noreply.github.com>
2023-08-22 14:16:12 +0200

    Add files via upload

---
+++ Main.r
@@ -0,0 +1,30 @@
+library(DBI)
+library(odbc)
+library(dplyr)
+library(SuperLearner)
+con <- dbConnect(odbc::odbc(), .connection_string = "Driver={SQL Server};Server=ka0327.loods2.org;Trusted_Connection=True;Database=SBX210", timeout = 10)
+ScriptLocation <- "L:/afdelingen/Actuariaat_Business/DennisS/PhD/A4/Scripts/R/"
+
+CompleteDataset <- tbl(con, "DS_PhD_CompleteSetA4_90")
+
+TimePoints <- as.data.frame(CompleteDataset %>% 
+  filter(dag90 < 3000) %>%
+  distinct(dag90) %>%
+  arrange(dag90))$dag90
+
+Medications <- c("C03AA","C03DA","C07A","C08","C09A","C09C","C10AA","C10AZ")
+
+source(paste0(ScriptLocation,"ExtraSuperLearners.r"))
+SLvector <- c("SL.glmnet","SL.glmnet.ridge","SL.randomForest","SL.stepAIC","SL.gam2")
+
+#source(paste0(ScriptLocation,"SwitchModels.r"))
+source(paste0(ScriptLocation,"CostGroupModels.r"))
+source(paste0(ScriptLocation,"PolisModels.r"))
+source(paste0(ScriptLocation,"ComorbiditieModels.r"))
+source(paste0(ScriptLocation,"MedicationUserModels.r"))
+source(paste0(ScriptLocation,"MedicationPickUpModels.r"))
+source(paste0(ScriptLocation,"MedicationPayingModels.r"))
+source(paste0(ScriptLocation,"MedicationAdherenceModels.r"))
+source(paste0(ScriptLocation,"CVEModels.r"))
+source(paste0(ScriptLocation,"DeathModels.r"))
+source(paste0(ScriptLocation,"CensorModels.r"))
